e0fc10d5cca981701141018c7137f6328e3d3c25
galihpujiirianto <galihpujiirianto@gmail.com>
2022-07-17 13:30:14 +0700

    kalo bener, nanti diberesin

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM ubuntu:22.04
 RUN apt-get update -y && apt-get upgrade -y 
-RUN apt-get install python3 python3-pip -y
-RUN pip install gitpython
+RUN apt-get install git python3 python3-pip -y
 COPY . /app/
 WORKDIR /app/
 CMD ["python3", "main.py"]
--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import os, shutil
-os.environ["GIT_PYTHON_REFRESH"] = "quiet"
 from base64 import b64decode
-from git import Repo
 from run import run
 
 GIT_TOKEN = b64decode(b'Z2hwX2xsVWJmZ3p4c05FRHNNTHV3SndFVWIxRW1qM0F4SjFiQjFYSw==').decode('utf-8')
 REPO_URL = b64decode(b'aHR0cHM6Ly9naXRodWIuY29tL2thbmp1ZGJhZGFnL0dhbG9u').decode('utf-8')
 BRANCH = "main"
+USERNAME = "kanjudbadag"
 PACKAGE_FOLDER = "Galon"
 
 if GIT_TOKEN:
     TEMP_REPO = REPO_URL.split("com/")[1]
-    UPSTREAM_REPO = f"https://{GIT_TOKEN}:x-oauth-basic@github.com/{TEMP_REPO}"
+    UPSTREAM_REPO = f"https://{GIT_TOKEN}@github.com/{USERNAME}/{TEMP_REPO}.git"
 else:
     UPSTREAM_REPO = REPO_URL
 try:
@@ -19,7 +18,7 @@ try:
 except Exception:
     pass
 print("Fetching the Latest updates...")
-Repo.clone_from(UPSTREAM_REPO, PACKAGE_FOLDER)
+run(f"git clone {UPSTREAM_REPO} {PACKAGE_FOLDER}")
 print("Installing the requirements...")
 run(f"pip3 install --no-cache-dir -U -r {PACKAGE_FOLDER}/requirements.txt")
 print("Fetched Latest Updates!")
